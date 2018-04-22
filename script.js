d2dc0320e53ecc277a873ac909c9e0d8cf4aab51
KamilaWalkowiak <kamila.bartkow@gmail.com>
2018-04-22 10:58:56 +0200

    Add OOP.

--- script.js
+++ script.js
@@ -1,38 +1,77 @@
-var personDb = [];
-
-function PersonDb() {
-    this.database =[];
-}
-
-PersonDb.prototype.addPerson = function (person) {
-console.log("addPerson");
-this.database.push(person);
+// Class DB
+function PersonDB() {
+    this.database = [];
 };
 
-PersonDb.prototype.displayPeople = function () {
+PersonDB.prototype.addPerson = function(person) {
+    this.database.push(person);
+};
+
+PersonDB.prototype.displayPeople = function() {
     console.log(this.database);
 };
 
-var dbInstance = new PersonDb();
 
+// Class People
 function Person(attributes) {
     this.name = attributes.name;
     this.surname = attributes.surname;
     this.age = attributes.age;
     this.role = attributes.role;
-}
+};
+
+
+// Class for render methods
+function Render(attribute) {
+    this.container = document.getElementById(attribute);
+};
+
+Render.prototype.renderRow = function(name, surname, age, role) {
+    return '<div><div>' + name + '</div><div>' + surname + '</div><div>' + age + '</div><div>' + role + '</div></div>';
+};
+
+Render.prototype.setHtml = function(database) {
+    console.log('database', this.container);
+    this.container.innerHTML = '';
+    var self = this;
+    database.map(function(person){
+        self.container.innerHTML += this.renderRow(person.name, person.surname, person.age, person.role);
+    });
+};
+
+// Create object of PersonDB
+var dbInstance = new PersonDB();
+
+// Create object of Render
+var renderInstance = new Render('records');
+
+
+renderInstance.setHtml(dbInstance.database);
+
+
+
+
+
+
+
+
+
+
+
 
 function renderRow(name, surname, age, role) {
     return '<div><div>' + name + '</div><div>' + surname + '</div><div>' + age + '</div><div>' + role + '</div></div>';
-}
+};
 
 function renderDb() {
     var containerDb = document.getElementById('records');
     containerDb.innerHTML = '';
-   dbInstance.database.map(function(person){
+    dbInstance.database.map(function(person){
         containerDb.innerHTML += renderRow(person.name, person.surname, person.age, person.role);
     });
-}
+};
+
+
 
 var addButton = document.getElementById('addButton');
 addButton.addEventListener('click', function(){
@@ -48,10 +87,7 @@ addButton.addEventListener('click', function(){
         role: inputRole
     });
 
-   dbInstance.addPerson(person1);
-
-    renderDb();
+    dbInstance.addPerson(person1);
+    console.log('click', dbInstance.database);
+    renderInstance.setHtml(dbInstance.database);
 });
-
-
-renderDb();
